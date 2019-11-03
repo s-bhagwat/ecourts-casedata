3f95949efaa12388bd88263a93fcbb04c5df740a
Thomas Bui <thomasbui198@gmail.com>
2019-11-02 22:44:27 -0700

    Added a line in captcha.py to write data into a jsonFile

--- captcha.py
+++ captcha.py
@@ -16,13 +16,13 @@ driver = None
 # start selenium here and open pages using the url from data
 def open_page_using_url(data):
     global driver
-    driver = webdriver.Firefox('./geckodriver')
-
+    driver = webdriver.Firefox()
+    data = data[:10]
     for block in data:
         driver.get(block['url'])
         # fill out form with "Court Complex" option
         block['courtComplex'] = fill_out_form(COURT_TYPE_COMPLEX)
-        # fill out form with "Court Complex" option
+        # fill out form with "Court Establishment" option
         driver.find_element_by_id('radCourtEst').click()
         block['courtEstablishment'] = fill_out_form(COURT_TYPE_ESTABLISHMENT)
     return data
@@ -65,25 +65,27 @@ def loop_over_case_type_options(data_array):
     fill_search_year_input()
     case_type_dropdown = driver.find_element_by_id('case_type')
     case_type_options = case_type_dropdown.find_elements_by_xpath(".//*")
-    for option_index in range(len(case_type_options)):
-        # skip default option
-        if option_index == 0:
-            continue
-        s_first_dropdown = Select(case_type_dropdown)
-        s_first_dropdown.select_by_index(option_index)
-        data = {
-            'val': case_type_options[option_index].get_attribute('value'),
-            'type': case_type_options[option_index].text
-        }
-        data_array.append(data)
-        try:
-            WebDriverWait(driver, 15).until(
-                expected_conditions.presence_of_element_located((By.XPATH, "//td[contains(@class, 'col-xs')]"))
-            )
-            loop_through_table()
-        except TimeoutException as py_ex:
-            print(py_ex)
-            continue
+    with open("scannedData.json", mode="w") as jsonFile:
+        for option_index in range(len(case_type_options)):
+            # skip default option
+            if option_index == 0:
+                continue
+            s_first_dropdown = Select(case_type_dropdown)
+            s_first_dropdown.select_by_index(option_index)
+            data = {
+                'val': case_type_options[option_index].get_attribute('value'),
+                'type': case_type_options[option_index].text
+            }
+            data_array.append(data)
+            jsonFile.write(str(data))
+            try:
+                WebDriverWait(driver, 15).until(
+                    expected_conditions.presence_of_element_located((By.XPATH, "//td[contains(@class, 'col-xs')]"))
+                )
+                loop_through_table()
+            except TimeoutException as py_ex:
+                print(py_ex)
+                continue
 
 
 # set the year
@@ -101,7 +103,6 @@ def loop_through_table():
     rows = tbody.find_elements_by_xpath(".//tr")
 
     d = {}
-
     for x in range(1, len(rows)):
         last_cell = rows[x].find_elements_by_xpath(".//td[contains(@class, 'col-xs-2')]")
         btn = last_cell[0].find_elements_by_xpath(".//a")[0]
@@ -115,7 +116,7 @@ def loop_through_table():
         if x == 3:
             break
 
-    print(d)
+    return d
 
 
 # get only the first url for testing purpose
